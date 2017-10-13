c94d53b2d7a1808f9ed5a9d0af8f78364336da57
deston-trace <destontrace@gmail.com>
2017-10-13 22:41:47 +0500

    search functionality added

--- app/models/property.rb
+++ app/models/property.rb
@@ -4,6 +4,27 @@ class Property < ApplicationRecord
   geocoded_by :full_street_address_display   # can also be an IP address
   after_validation :geocode          # auto-fetch coordinates
 
+  def self.search(search)
+    if search
+      self.where("house_num LIKE ?
+                  OR town LIKE ?
+                  OR city LIKE ?
+                  OR country LIKE ?
+                  OR propertytype LIKE ?
+                  OR price_for_sale LIKE ?
+                  OR price_for_rent LIKE ?",
+                  "%#{search}%",
+                  "%#{search}%",
+                  "%#{search}%",
+                  "%#{search}%",
+                  "%#{search}%",
+                  "%#{search}%",
+                  "%#{search}%")
+    else
+      self.all
+    end
+  end
+
   def full_street_address
     return "#{town} #{city} #{country}"
   end
