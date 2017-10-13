969e46c2d636e441debb92d6ce48f72915dfc5c4
deston-trace <destontrace@gmail.com>
2017-10-13 21:03:15 +0500

    fix small issues

--- app/models/property.rb
+++ app/models/property.rb
@@ -1,7 +1,7 @@
 class Property < ApplicationRecord
   belongs_to :user
 
-  geocoded_by :full_street_address   # can also be an IP address
+  geocoded_by :full_street_address_display   # can also be an IP address
   after_validation :geocode          # auto-fetch coordinates
 
   def full_street_address
